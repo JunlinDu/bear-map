e95bc8bda72beeef4b6c4ef445221f8feac18276
Junlin Du <matthewdu7@gmail.com>
2021-03-19 16:03:15 +1100

    function correctly returns coordinate

--- src/main/java/Rasterer.java
+++ src/main/java/Rasterer.java
@@ -4,9 +4,8 @@ import java.util.ArrayList;
 
 /**
  * This class provides all code necessary to take a query box and produce
- * a query result. The getMapRaster method must return a Map containing all
- * seven of the required fields, otherwise the front end code will probably
- * not draw the output correctly.
+ * a query result.
+ * @author Junlin Du
  */
 public class Rasterer {
 
@@ -18,9 +17,9 @@ public class Rasterer {
         lonCoverage = MapServer.ROOT_LRLON - MapServer.ROOT_ULLON;
         latCoverage = MapServer.ROOT_ULLAT - MapServer.ROOT_LRLAT;
         zoomLevelLonDPPs = new ArrayList<Double>();
-        // [3.4332275390625E-4, 1.71661376953125E-4, 8.58306884765625E-5,
-        // 4.291534423828125E-5, 2.1457672119140625E-5, 1.0728836059570312E-5,
-        // 5.364418029785156E-6, 2.682209014892578E-6]
+        // LonDPP of 8 levels of tile:
+        // [3.4332275390625E-4, 1.71661376953125E-4, 8.58306884765625E-5, 4.291534423828125E-5,
+        // 2.1457672119140625E-5, 1.0728836059570312E-5, 5.364418029785156E-6, 2.682209014892578E-6]
 
         for (int i = 0, l = 1; i < 8; i++, l*=2) {
             // calculates and initiates an array list for the LonDPP of 8 levels of zoom
@@ -40,6 +39,56 @@ public class Rasterer {
             i++;
         }
         return i;
+    }
+
+    /**
+     * This function search for the longitude or latitude of the appropriate
+     * tile based on the current zoom level and a given coordinate.
+     * This is a recursive function utilizes the principles of binary
+     * search.
+     *
+     * @param depth the current level of zoom (depth)
+     * @param coordinate the coordinate of the query box
+     * @param lowerBound the coordinate of the lower bound of the bounding box for a single dimension
+     * @param upperBound the coordinate of the upper bound of the bounding box for a single dimension
+     *
+     * @return the coordinate of a rastered tile
+     *
+    * */
+    private double searchSingleDimCoord(int depth, double coordinate, double lowerBound, double upperBound) {
+
+        if (depth != 0) {
+            double mid = lowerBound + (upperBound - lowerBound) / 2;
+            if (coordinate == mid) return mid;
+
+            if (coordinate < mid)
+                return searchSingleDimCoord(depth - 1, coordinate, lowerBound, mid);
+
+            return searchSingleDimCoord(depth - 1, coordinate, mid, upperBound);
+        }
+
+        return lowerBound;
+    }
+
+
+    private int calcULTileCoord(int depth) {
+
+        return 0;
+    }
+
+
+    private int calcLRTileCoord() {
+
+        return 0;
+    }
+
+
+    private Map<String, Double> calcRasterCoord(double ulLon, double ulLat, double lrLon, double lrLat) {
+        return null;
+    }
+
+    private String[][] constructTile() {
+        return null;
     }
 
     /**
@@ -80,6 +129,24 @@ public class Rasterer {
         System.out.println("Depth: " + String.valueOf(calcDepth(lonDPP)));
         Map<String, Object> results = new HashMap<>();
         return results;
+    }
+
+
+    /** The Main function is used for testing purposes.*/
+    public static void main (String[] args) {
+        Rasterer rasterer = new Rasterer();
+        double result = rasterer.searchSingleDimCoord(3, 4.2, 0.0, 16.0);
+        System.out.println(result); // 4
+        result = rasterer.searchSingleDimCoord(3, 5.2, 0.0, 16.0);
+        System.out.println(result);
+        result = rasterer.searchSingleDimCoord
+                (7, -122.24163047377972,
+                        MapServer.ROOT_ULLON, MapServer.ROOT_LRLON);
+        System.out.println(result); // expected: -122.24212646484375
+        result = rasterer.searchSingleDimCoord
+                (1, -122.3027284165759,
+                        MapServer.ROOT_ULLON, MapServer.ROOT_LRLON);
+        System.out.println(result); // expected: -122.2998046875
     }
 
 }
