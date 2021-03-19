f358b7263ed1b8c0eccdca70edf28faaf78c7b40
Junlin Du <matthewdu7@gmail.com>
2021-03-19 17:34:27 +1100

    raster tile coordinate calculation done

--- src/main/java/Rasterer.java
+++ src/main/java/Rasterer.java
@@ -1,6 +1,7 @@
 import java.util.HashMap;
 import java.util.Map;
 import java.util.ArrayList;
+import java.lang.Math;
 
 /**
  * This class provides all code necessary to take a query box and produce
@@ -11,15 +12,17 @@ public class Rasterer {
 
     private double lonCoverage;
     private double latCoverage;
+    /*
+     * For reference and testing purposes. LonDPP of 8 levels of tile:
+     * [3.4332275390625E-4, 1.71661376953125E-4, 8.58306884765625E-5, 4.291534423828125E-5,
+     * 2.1457672119140625E-5, 1.0728836059570312E-5, 5.364418029785156E-6, 2.682209014892578E-6]
+     */
     private ArrayList<Double>  zoomLevelLonDPPs;
 
     public Rasterer() {
         lonCoverage = MapServer.ROOT_LRLON - MapServer.ROOT_ULLON;
         latCoverage = MapServer.ROOT_ULLAT - MapServer.ROOT_LRLAT;
         zoomLevelLonDPPs = new ArrayList<Double>();
-        // LonDPP of 8 levels of tile:
-        // [3.4332275390625E-4, 1.71661376953125E-4, 8.58306884765625E-5, 4.291534423828125E-5,
-        // 2.1457672119140625E-5, 1.0728836059570312E-5, 5.364418029785156E-6, 2.682209014892578E-6]
 
         for (int i = 0, l = 1; i < 8; i++, l*=2) {
             // calculates and initiates an array list for the LonDPP of 8 levels of zoom
@@ -45,7 +48,9 @@ public class Rasterer {
      * This function search for the longitude or latitude of the appropriate
      * tile based on the current zoom level and a given coordinate.
      * This is a recursive function utilizes the principles of binary
-     * search.
+     * search. The upperBound and lowerBound can be thought of as two ends
+     * of a sorted array, where the depth determines the number of elements
+     * in the array.
      *
      * @param depth the current level of zoom (depth)
      * @param coordinate the coordinate of the query box
@@ -59,32 +64,52 @@ public class Rasterer {
 
         if (depth != 0) {
             double mid = lowerBound + (upperBound - lowerBound) / 2;
+
+            // If the coordinate happens to coincide with the mid point
+            // This may happen very rarely.
             if (coordinate == mid) return mid;
 
+            // If the coordinate is smaller than the mid point, i.e.
+            // is on the left side of the imaginary array.
             if (coordinate < mid)
                 return searchSingleDimCoord(depth - 1, coordinate, lowerBound, mid);
 
+            // The only possibility left is that the coordinate is
+            // greater that the mid point.
             return searchSingleDimCoord(depth - 1, coordinate, mid, upperBound);
         }
 
+        // This will be the coordinate of the rastered tile.
+        // The coordinate is the greatest of an array (imaginary)
+        // of tiles that is smaller than the given coordinate,
+        // thus have the requested region covered.
         return lowerBound;
     }
 
-
-    private int calcULTileCoord(int depth) {
-
-        return 0;
+    /**
+    * This function returns the longitudinal and latitudinal
+    * coordinates of a tile.
+    *
+    * @param depth current level of zoom
+    * @param longitude requested longitude
+     * @param latitude requested latitude
+     *
+     * @return a double array: [tileUlLon, tileUlLat]
+    * */
+    private double[] calcTileULCoor(int depth, double longitude, double latitude) {
+        // The longitudinal/latitudinal distance a tile covers
+        double tileCoverage = this.latCoverage / Math.pow(2, depth);
+        return new double[]{
+                searchSingleDimCoord(depth, longitude, MapServer.ROOT_ULLON, MapServer.ROOT_LRLON),
+                searchSingleDimCoord(depth, latitude, MapServer.ROOT_LRLAT, MapServer.ROOT_ULLAT)
+                        + tileCoverage};
     }
 
-
+    /*
+     * [raster_lr_lon, raster_lr_lat]*/
     private int calcLRTileCoord() {
 
         return 0;
-    }
-
-
-    private Map<String, Double> calcRasterCoord(double ulLon, double ulLat, double lrLon, double lrLat) {
-        return null;
     }
 
     private String[][] constructTile() {
@@ -95,16 +120,6 @@ public class Rasterer {
      * Takes a user query and finds the grid of images that best matches the query. These
      * images will be combined into one big image (rastered) by the front end. <br>
      *
-     *     The grid of images must obey the following properties, where image in the
-     *     grid is referred to as a "tile".
-     *     <ul>
-     *         <li>The tiles collected must cover the most longitudinal distance per pixel
-     *         (LonDPP) possible, while still covering less than or equal to the amount of
-     *         longitudinal distance per pixel in the query box for the user viewport size. </li>
-     *         <li>Contains all tiles that intersect the query bounding box that fulfill the
-     *         above condition.</li>
-     *         <li>The tiles must be arranged in-order to reconstruct the full image.</li>
-     *     </ul>
      *
      * @param params Map of the HTTP GET request's query parameters - the query box and
      *               the user viewport width and height.
@@ -123,7 +138,8 @@ public class Rasterer {
 
         // prints out the HTTP GET request's query parameters
         System.out.println(params);
-        // TODO print out the LonDPP of the query Box
+
+
         double lonDPP  = (params.get("lrlon") - params.get("ullon"))/ params.get("w");
         System.out.println("LonDPP: " + lonDPP);
         System.out.println("Depth: " + String.valueOf(calcDepth(lonDPP)));
@@ -135,9 +151,9 @@ public class Rasterer {
     /** The Main function is used for testing purposes.*/
     public static void main (String[] args) {
         Rasterer rasterer = new Rasterer();
-        double result = rasterer.searchSingleDimCoord(3, 4.2, 0.0, 16.0);
+        double result = rasterer.searchSingleDimCoord(4, 4.2, 0.0, 16.0);
         System.out.println(result); // 4
-        result = rasterer.searchSingleDimCoord(3, 5.2, 0.0, 16.0);
+        result = rasterer.searchSingleDimCoord(4, 5.2, 0.0, 16.0);
         System.out.println(result);
         result = rasterer.searchSingleDimCoord
                 (7, -122.24163047377972,
@@ -147,6 +163,33 @@ public class Rasterer {
                 (1, -122.3027284165759,
                         MapServer.ROOT_ULLON, MapServer.ROOT_LRLON);
         System.out.println(result); // expected: -122.2998046875
+        result = rasterer.searchSingleDimCoord
+                (2, -122.30410170759153,
+                        MapServer.ROOT_ULLON, MapServer.ROOT_LRLON);
+        System.out.println(result); // expected: -122.2998046875
+
+        result = rasterer.searchSingleDimCoord
+                (1, 37.88708748276975, MapServer.ROOT_ULLAT, MapServer.ROOT_LRLAT);
+        System.out.println(result); // expected: 37.85749899038596
+        double a = (MapServer.ROOT_ULLAT - MapServer.ROOT_LRLAT) / 2;
+        System.out.println(37.85749899038596 + a); // expected: 37.892195547244356
+
+        System.out.println(rasterer.latCoverage);
+        System.out.println("###################");
+
+        double[] coord = rasterer.calcTileULCoor(1, -122.3027284165759, 37.88708748276975);
+        System.out.println(coord[0]); //expected: -122.2998046875
+        System.out.println(coord[1]); //expected: 37.892195547244356
+
+        System.out.println("###################");
+        coord = rasterer.calcTileULCoor(7, -122.24163047377972, 37.87655856892288);
+        System.out.println(coord[0]); //expected: -122.24212646484375
+        System.out.println(coord[1]); //expected: 37.87701580361881
+
+        System.out.println("###################");
+        coord = rasterer.calcTileULCoor(2, -122.30410170759153, 37.870213571328854);
+        System.out.println(coord[0]); //expected: -122.2998046875
+        System.out.println(coord[1]); //expected: 37.87484726881516
     }
 
 }
