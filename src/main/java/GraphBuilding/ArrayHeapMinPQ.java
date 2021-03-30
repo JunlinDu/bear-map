d07462721a651098fbd792f59cdd1ec7dfddfd8c
Junlin Du <matthewdu7@gmail.com>
2021-03-30 15:25:44 +1100

    adding previously implemented PQ for performing A*

---
+++ src/main/java/GraphBuilding/ArrayHeapMinPQ.java
@@ -0,0 +1,259 @@
+package GraphBuilding;
+
+import java.util.ArrayList;
+import java.util.NoSuchElementException;
+import java.util.TreeMap;
+
+/* Implementation of the Priority Queue, implemented on Jul 24, 2020
+* @author Junlin Du
+* */
+public class ArrayHeapMinPQ<I> implements ExtrinsicMinPQ<I> {
+
+    private class Node implements Comparable<Node>{
+
+        /* The item being Stored in the object */
+        I item;
+
+        /* The Priority Value of the object */
+        double priority;
+
+        Node(I item, double priority) {
+            this.item = item;
+            this.priority = priority;
+        }
+
+        /* Item Getter */
+        public I getItem() {
+            return item;
+        }
+
+        /* Priority Value Getter */
+        public double getPriority() {
+            return priority;
+        }
+
+        /* Priority Value Setter */
+        public void setPriority(double priority) {
+            this.priority = priority;
+        }
+
+        /**
+         * Compares this Node with the specified Node for order.  Returns a
+         * negative integer, zero, or a positive integer as this Node is less
+         * than, equal to, or greater than the specified Node. */
+        @Override
+        public int compareTo(Node o) {
+            int a = 0;
+            if (this.getPriority() > o.getPriority()) a++;
+            if (this.getPriority() < o.getPriority()) a--;
+            return a;
+        }
+    }
+
+    /* The Arraylist that represents the Min Heap */
+    private ArrayList<Node> minHeap;
+
+    private TreeMap<I, Integer> keySet;
+
+    public ArrayHeapMinPQ() {
+        minHeap = new ArrayList<>();
+        minHeap.add(new Node(null, 0.0));
+        keySet = new TreeMap<>();
+    }
+
+    /* return the index of the parent node of the specified node index */
+    private int getParent(int index) {
+        return index / 2;
+    }
+
+    /* return the index of the left child of the specified node index */
+    private int leftChild(int index) {
+        return index * 2;
+    }
+
+    /* return the index of the right child of the specified node index */
+    private int rightChild(int index) {
+        return index *2 + 1;
+    }
+
+    /* return true if the first argument is less than the second argument, false otherwise*/
+    private boolean lessThan(int i, int j) {
+        return this.minHeap.get(i).compareTo(this.minHeap.get(j)) < 0;
+    }
+
+    /* Swap the child node at the provided index with its parentNode */
+    private void swap(int childIndex, int parentIndex) {
+        Node temp = this.minHeap.get(parentIndex);
+        this.minHeap.set(parentIndex, this.minHeap.get(childIndex));
+        this.minHeap.set(childIndex, temp);
+        this.keySet.put(this.minHeap.get(childIndex).item, childIndex);
+        this.keySet.put(this.minHeap.get(parentIndex).item, parentIndex);
+    }
+
+    /* Promote a node up the hierarchy to the position that it belongs to */
+    private void swim(int index) {
+        int parentIndex = getParent(index);
+        if (index != 0 && lessThan(index, parentIndex)) {
+            swap(index, parentIndex);
+            swim(parentIndex);
+        }
+        return;
+    }
+
+    /* Demote a node down the hierarchy to the position that it belongs to */
+    private void sink(int index) {
+        if (leftChild(index) > size() || rightChild(index) > size()) return;
+        if (lessThan(leftChild(index), index) || lessThan(rightChild(index), index)) {
+            if (lessThan(leftChild(index), rightChild(index))) {
+                swap(leftChild(index), index);
+                sink(leftChild(index));
+            } else {
+                swap(rightChild(index), index);
+                sink(rightChild(index));
+            }
+        }
+    }
+
+    /* Add a node */
+    @Override
+    public void add(I item, double priority) {
+        if (contains(item)) throw new IllegalArgumentException();
+
+        this.minHeap.add(new Node(item, priority));
+        this.keySet.put(item, size());
+        swim(this.minHeap.size() - 1);
+    }
+
+    /* Checks to see if the PQ contains a given item, return true
+     *  if it does, false otherwise */
+    @Override
+    public boolean contains(I item) {
+        if (item == null) throw new IllegalArgumentException();
+        return keySet.containsKey(item);
+    }
+
+    /* returns the the smallest item in the PQ */
+    @Override
+    public I getSmallest() {
+        if (this.minHeap.size() == 1) {
+            throw new NoSuchElementException();
+        }
+        return this.minHeap.get(1).getItem();
+    }
+
+    /* remove the smallest item from the PQ */
+    @Override
+    public I removeSmallest() {
+        if (this.minHeap.size() == 1) throw new NoSuchElementException();
+
+        this.keySet.remove(this.minHeap.get(1).item);
+        this.keySet.put(this.minHeap.get(size()).item,1);
+        this.minHeap.set(1, this.minHeap.get(size()));
+        this.minHeap.remove(size());
+        sink(1);
+        return size() == 0? null : getSmallest();
+    }
+
+    /* return the size of the PQ */
+    @Override
+    public int size() {
+        return this.minHeap.size() - 1;
+    }
+
+    /* Change the priority of an Item */
+    @Override
+    public void changePriority(I item, double priority) {
+        if (!keySet.containsKey(item)) {
+            throw new NoSuchElementException();
+        }
+
+        int index = this.keySet.get(item);
+        Node node = this.minHeap.get(index);
+        node.setPriority(priority);
+        if (lessThan(index, getParent(index))) swim(index);
+        else sink(index);
+    }
+
+
+    /*
+     * *****Testing Purposes only ******
+     * *****Testing Purposes only ******
+     * *****Testing Purposes only ******
+     * */
+    public static void main(String[] args) {
+        ArrayHeapMinPQ<String> pq = new ArrayHeapMinPQ<>();
+        pq.add("1", 1.0);
+        pq.removeSmallest();
+        pq.add("2", 2.0);
+        pq.add("3", 3.0);
+        pq.add("4", 4.0);
+        pq.add("5", 5.0);
+        pq.add("6", 6.0);
+        pq.add("7", 7.0);
+        pq.add("0", 0);
+        pq.removeSmallest();
+        pq.changePriority("4", 0);
+
+        String[] arr = new String[pq.minHeap.size()];
+        for (int i = 0; i < pq.minHeap.size(); i++) {
+            arr[i] = pq.minHeap.get(i).item;
+        }
+
+        printFancyHeapDrawing(arr);
+    }
+
+    public static void printSimpleHeapDrawing(Object[] heap) {
+        int depth = ((int) (Math.log(heap.length) / Math.log(2)));
+        int level = 0;
+        int itemsUntilNext = (int) Math.pow(2, level);
+        for (int j = 0; j < depth; j++) {
+            System.out.print(" ");
+        }
+
+        for (int i = 1; i < heap.length; i++) {
+            System.out.printf("%d ", heap[i]);
+            if (i == itemsUntilNext) {
+                System.out.println();
+                level++;
+                itemsUntilNext += Math.pow(2, level);
+                depth--;
+                for (int j = 0; j < depth; j++) {
+                    System.out.print(" ");
+                }
+            }
+        }
+        System.out.println();
+    }
+
+    /** Prints out a drawing of the given array of Objects assuming it
+     *  is a heap starting at index 1. You're welcome to copy and paste
+     *  code from this method into your code, just make sure to cite
+     *  this with the @source tag. */
+    public static void printFancyHeapDrawing(Object[] items) {
+        String drawing = fancyHeapDrawingHelper(items, 1, "");
+        System.out.println(drawing);
+    }
+
+    /* Recursive helper method for toString. */
+    private static String fancyHeapDrawingHelper(Object[] items, int index, String soFar) {
+        if (index >= items.length || items[index] == null) {
+            return "";
+        } else {
+            String toReturn = "";
+            int rightIndex = 2 * index + 1;
+            toReturn += fancyHeapDrawingHelper(items, rightIndex, "        " + soFar);
+            if (rightIndex < items.length && items[rightIndex] != null) {
+                toReturn += soFar + "    /";
+            }
+            toReturn += "\n" + soFar + items[index] + "\n";
+            int leftIndex = 2 * index;
+            if (leftIndex < items.length && items[leftIndex] != null) {
+                toReturn += soFar + "    \\";
+            }
+            toReturn += fancyHeapDrawingHelper(items, leftIndex, "        " + soFar);
+            return toReturn;
+        }
+    }
+
+}
+
