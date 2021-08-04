7ab1d4fb365c9642bace11eaac286ab10bb07bee
Junlin Du <matthewdu7@gmail.com>
2021-08-05 03:18:59 +1000

    router handler route added

---
+++ src/main/java/controller/impl/RouterHandler.java
@@ -0,0 +1,40 @@
+package controller.impl;
+
+import Router.Router;
+import controller.RouteHandler;
+import spark.Request;
+import spark.Response;
+import utils.Constants;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static utils.TextFormatter.getDirectionsText;
+import static utils.Constants.graph;
+import static utils.Constants.ROUTES;
+
+public class RouterHandler extends RouteHandler<Map<String, Double>, Map<String, Object>> {
+    @Override
+    protected Map<String, Double> parseRequestParams(Request req) {
+        return getRequestParams(req, Constants.REQUIRED_ROUTE_REQUEST_PARAMS);
+    }
+
+    @Override
+    protected Map<String, Object> processRequest(Map<String, Double> params, Response res) {
+        ROUTES.addAll(Router.shortestPath(graph,
+                        params.get("start_lon"),
+                        params.get("start_lat"),
+                        params.get("end_lon"),
+                        params.get("end_lat")));
+
+        String directions = getDirectionsText(graph, ROUTES);
+
+        Map<String, Object> routeParams = new HashMap<>();
+        routeParams.put("routing_success", !ROUTES.isEmpty());
+        routeParams.put("directions_success", directions.length() > 0);
+        routeParams.put("directions", directions);
+
+        return routeParams;
+    }
+}
---
+++ src/main/java/utils/TextFormatter.java
@@ -0,0 +1,28 @@
+package utils;
+
+import GraphBuilder.GraphDB;
+import Router.Router;
+
+import java.util.List;
+
+public class TextFormatter {
+    /**
+     * Takes the route of the server and converts it into an HTML friendly
+     * String to be passed to the frontend.
+     */
+    public static String getDirectionsText(GraphDB graph, List<Long> route) {
+
+        List<Router.NavigationDirection> directions = Router.routeDirections(graph, route);
+
+        if (directions == null || directions.isEmpty()) {
+            return "";
+        }
+        StringBuilder sb = new StringBuilder();
+        int step = 1;
+        for (Router.NavigationDirection d: directions) {
+            sb.append(String.format("%d. %s <br>", step, d));
+            step += 1;
+        }
+        return sb.toString();
+    }
+}
