c20c11fda2d7c75d8950f34fbedd863cec50c46c
Junlin Du <matthewdu7@gmail.com>
2021-08-05 03:38:35 +1000

    Added clear route route

--- src/main/java/controller/impl/ClearRouteHandler.java
+++ src/main/java/controller/impl/ClearRouteHandler.java
@@ -1,2 +1,20 @@
-package controller.impl;public class ClearRouteHandler {
+package controller.impl;
+
+import controller.RouteHandler;
+import spark.Request;
+import spark.Response;
+
+import static utils.Constants.ROUTES;
+
+public class ClearRouteHandler extends RouteHandler {
+    @Override
+    protected Object parseRequestParams(Request req) {
+        return null;
+    }
+
+    @Override
+    protected Object processRequest(Object params, Response res) {
+        ROUTES.clear();
+        return true;
+    }
 }
